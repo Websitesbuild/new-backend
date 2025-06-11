5d0b16a532240b54ed9a81bb4a583bc01f8621b6
Amit Sharma <amitsharma691999@gmail.com>
2025-06-11 12:21:18 +0530

    Your commit message here

--- index.js
+++ index.js
@@ -85,7 +85,8 @@ passport.use(new LocalStrategy({
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:5000/auth/google/callback",
+  // callbackURL: "http://localhost:5000/auth/google/callback",
+  callbackURL: "https://new-backend-3jbn.onrender.com/auth/google/callback",
   passReqToCallback: true,
 }, async (request, accessToken, refreshToken, profile, done) => {
   try {
